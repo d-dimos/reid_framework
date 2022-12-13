1e4c2194c0fa19550c163c868e05e562499e4be2
Dimitris Dimos <dimitris.dimos647@gmail.com>
2022-12-13 14:15:11 +0200

    minor change

--- torchreid_uncertain/models/osnet_uain.py
+++ torchreid_uncertain/models/osnet_uain.py
@@ -446,7 +446,7 @@ class OSNet(nn.Module):
         # conv_var_cat = self.conv1x1_param(conv_var_cat)
         # conv_var_cat = self.global_avgpool_param(conv_var_cat)
 
-        absmo = self.fc1var(conv_var_cat.detach())
+        absmo = self.fc1var(conv_var_cat) #.detach())
         a, b, smo = torch.split(absmo, 1, dim=1)
         smo = self.sig1var(smo)
         m = Bernoulli(smo)
